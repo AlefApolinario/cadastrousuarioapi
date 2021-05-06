861124ed6fec45283070d5be8218520da47d62f7
Alef Apolinario <alefcristiangomes@gmail.com>
2021-05-06 16:02:25 -0300

    commit projeto scaffold

---
+++ cadastrousuarioapirest/src/main/java/com/github/alef/apolinario/cadastrousuarioapi/CadastrousuarioapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.alef.apolinario.cadastrousuarioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrousuarioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrousuarioapiApplication.class, args);
+	}
+
+}
---
+++ cadastrousuarioapirest/src/test/java/com/github/alef/apolinario/cadastrousuarioapi/CadastrousuarioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alef.apolinario.cadastrousuarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrousuarioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
